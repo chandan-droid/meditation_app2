034fd2cedba8d81ff6714ce43b00377845fb64bb
dev <147903617+chandan-droid@users.noreply.github.com>
2024-02-10 19:27:47 +0530

    ygu

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MeditationApp());
+}
+
+class MeditationApp extends StatelessWidget{
+
+  // MeditationApp(){
+  //   print(s);
+  // }
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  };
+}
