f485f10ad9e6cc750122549d4f4d00f80203f378
dev <147903617+chandan-droid@users.noreply.github.com>
2024-02-11 10:22:15 +0530

    ygu

--- lib/main.dart
+++ lib/main.dart
@@ -28,23 +28,88 @@ class Homepage extends StatelessWidget{
   Widget build(BuildContext context){
     return Scaffold(
 
-      body: Container(
-        alignment:Alignment.center,
-        margin:const EdgeInsets.symmetric(vertical: 50.0, horizontal: 112.0,),
-        //color: Colors.grey,
-        height: 40,
-        width: 500,
-        child: Row(
-          children: <Widget>[
-            const Text('Silent',style: TextStyle(fontSize: 20,letterSpacing: 4,),),
-           mainIcon,
-            const Text('Moon',style: TextStyle(fontSize: 20,letterSpacing: 4,),),
-          ],
-        ),
-        //padding: const EdgeInsets.all(1.0),
-        //child: const Text('silent moon', style: TextStyle(fontSize: 30,letterSpacing: 2,),),
 
-      ),
+       body:Column(
+         crossAxisAlignment:  CrossAxisAlignment.start,
+         children: [
+           //head line
+           Container(
+             alignment:Alignment.center,
+             margin:const EdgeInsets.symmetric(vertical: 50.0, horizontal: 112.0,),
+             height: 30,
+             width: 500,
+             child: Row(
+               children: <Widget>[
+                 const Text('Silent',style: TextStyle(fontSize: 20,letterSpacing: 4,),),
+                 mainIcon,
+                 const Text('Moon',style: TextStyle(fontSize: 20,letterSpacing: 4,),),
+               ],
+             ),
+           ),
+
+           //good morning
+              Padding(
+                  padding:const EdgeInsets.only(left:20.0 ),
+                child: Container(
+                  child: const Text('Good Morning, Afsar',style: TextStyle(fontSize: 28,fontWeight: FontWeight.bold,color: Colors.black87),),
+                ),
+              ),
+           //wish
+             const  Padding(
+                  padding: EdgeInsets.only(left:20.0 ),
+                  child: Text('We wish you have a good day',style: TextStyle(fontSize: 20,color: Colors.black45),),
+              ),
+
+           SizedBox(height: 20,),
+           //course/music
+            Row(
+             children: [
+               Padding(
+                   padding:EdgeInsets.only(left:20.0 ),
+                   child: Container(
+                          height: 210,
+                          width: 177,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(10),
+                            color: Colors.blue.shade400
+                          ),
+                   ),
+               ),
+               Padding(
+                 padding:EdgeInsets.only(left:20.0 ),
+                 child: Container(
+                   height: 210,
+                   width: 177,
+                   decoration: BoxDecoration(
+                       borderRadius: BorderRadius.circular(10),
+                       color: Colors.yellow.shade700
+                   ),
+                 ),
+               )
+             ],
+           ),
+
+           SizedBox(height: 20,),
+
+           //daily thoughts
+           Padding(
+             padding:EdgeInsets.only(left:20.0 ),
+             child: Container(
+               height: 95,
+               width: 347,
+               decoration: BoxDecoration(
+                   borderRadius: BorderRadius.circular(10),
+                   color: Colors.yellow.shade700
+               ),
+             ),
+           )
+
+           //recommended for you
+
+         ],
+
+       ),
+
 
       bottomNavigationBar: BottomNavigationBar(
         type: BottomNavigationBarType.fixed,
